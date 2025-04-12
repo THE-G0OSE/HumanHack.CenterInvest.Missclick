d84956ead3b1b6afbfa98b721a33891128af6560
THE-G0OSE <nikitos54345@gmail.com>
2025-04-12 06:28:15 +0300

    fix

--- src/pages/auth/register/Register.tsx
+++ src/pages/auth/register/Register.tsx
@@ -6,7 +6,6 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
-import { Separator } from "@/components/ui/separator"
 import { Leaf } from "lucide-react"
 import { useNavigate } from "react-router"
 
